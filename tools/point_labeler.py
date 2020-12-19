1ea96de1f3b2a05aac61333c64c0136b953dd4c5
chenli <chenli@ainfinit.com>
2020-12-19 14:26:12 +0800

    backup

--- tools/point_labeler.py
+++ tools/point_labeler.py
@@ -5,7 +5,7 @@
 
 数据的layout请参考: lib.labeler.Labeler.
 
-左键单击选点, 左键按下拖动选择区域, 右键单击删除区域中的点.
+左键按下添加标注点, 鼠标拖动选择与鼠标最近的标注点, 右键单击删除选中的标注点.
 """
 
 import cv2
@@ -20,86 +20,80 @@ class PointLabeler(lib.labeler.Labeler):
     def __init__(self, root_dir, scale=1.0):
         super().__init__(root_dir)
         self.scale = scale
+        self.selected_point = None
 
-    def _load_curr_image(self):
-        image = super()._load_curr_image()
+    def _load_image(self, samples_id):
+        image = super()._load_image(samples_id)
         height, width = image.shape[:2]
         height = int(round(height * self.scale))
         width = int(round(width * self.scale))
         return cv2.resize(image, (width, height))
 
-    def _load_curr_annotations(self):
+    def _load_annotations(self, samples_id):
         annotations = []
-        for x, y in super()._load_curr_annotations():
+        content = super()._load_annotations(samples_id)
+        if not content: return annotations
+        for x, y in content:
             x = int(round(x * self.scale))
             y = int(round(y * self.scale))
             annotations.append((x, y))
         return annotations
 
-    def _save_curr_annotations(self):
-        annotations = []
+    def _save_annotations(self, annotations):
+        to_be_saved = []
         for x, y in self.curr_annotations:
             x = int(round(x / self.scale))
             y = int(round(y / self.scale))
-            annotations.append((x, y))
-        self.curr_annotations = annotations
-        super()._save_curr_annotations()
-
-    def _find_selected_points(self):
-        bbox = self.cache_data or lib.labeler.BoundingBox()
-        return [pt for pt in self.curr_annotations
-                if bbox.valid() and bbox.contains(point=pt)]
+            to_be_saved.append((x, y))
+        super()._save_annotations(to_be_saved)
 
     def _draw_curr_image(self):
         show = super()._draw_curr_image()
         for x, y in self.curr_annotations:
             cv2.circle(show, (x, y), 4, (0, 255, 255), thickness=-1)
-        for x, y in self._find_selected_points():
-            cv2.circle(show, (x, y), 8, (0, 0, 255), thickness=-1)
-        if self.cache_data and self.cache_data.valid():
-            pt1 = self.cache_data.top_left
-            pt2 = self.cache_data.bottom_right
-            cv2.rectangle(show, pt1, pt2, (0, 0, 255), thickness=2)
+        if self.selected_point:
+            cv2.circle(show, self.selected_point, 8, (0, 0, 255), thickness=-1)
         return show
 
+    def _get_closest_point(self, x, y):
+        if not self.curr_annotations: return None, None
+        distances = []
+        for x1, y1 in self.curr_annotations:
+            dist = (x1 - x) ** 2 + (y1 - y) ** 2
+            distances.append(((x1, y1), dist))
+        return min(distances, key=lambda v: v[-1])
+
     def _mouse_callback(self, event, x, y, flags):
-        # 左键按下, 选择一个点
+        # 左键按下, 添加一个标注点
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.cache_data = lib.labeler.BoundingBox()
-            self.cache_data.x1 = x
-            self.cache_data.y1 = y
-        # 左键抬起, 选择另一个点, 如果这个点和前一个点相同, 则为单击.
-        # 如果两个点不同, 则为拖动. 单击选点, 拖动选区域.
-        elif event == cv2.EVENT_LBUTTONUP:
-            self.cache_data.x2 = x
-            self.cache_data.y2 = y
-            if self.cache_data.x1 == x and self.cache_data.y1 == y:
+            point, dist = self._get_closest_point(x, y)
+            if point is None or dist > 1600:
                 self.curr_annotations.append((x, y))
-                self.cache_data = None
-            elif self.cache_data.area < 400:
-                self.cache_data = None
+        # 鼠标拖动, 选择一个标注点
         elif event == cv2.EVENT_MOUSEMOVE:
-            # 这个flag表示鼠标拖动时左键为按下状态
-            if flags == cv2.EVENT_FLAG_LBUTTON:
-                self.cache_data.x2 = x
-                self.cache_data.y2 = y
-        # 右键单击删除选中的点
+            self.selected_point = None
+            point, dist = self._get_closest_point(x, y)
+            if point is not None and dist < 1600:
+                self.selected_point = point
+        # 右键单击删除选中的标注点
         elif event == cv2.EVENT_RBUTTONDOWN:
-            for pt in self._find_selected_points():
-                self.curr_annotations.remove(pt)
-            self.cache_data = None
+            if self.selected_point:
+                self.curr_annotations.remove(self.selected_point)
+                self.selected_point = None
 
     def _key_callback(self, key):
         if key != 255 and key != -1:
             logging.info("Pressed key: %d", key)
         super()._key_callback(key)
-        if key == ord("d"):  # 按d删除选中的点
-            for pt in self._find_selected_points():
-                self.curr_annotations.remove(pt)
-            self.cache_data = None
+        if key == ord("d"):
+            if self.selected_point:
+                self.curr_annotations.remove(self.selected_point)
+                self.selected_point = None
+        elif key == ord("c"):
+            self.curr_annotations = []
 
 
 if __name__ == "__main__":
     lib.util.initialize_logger()
-    PointLabeler("../data", scale=0.75).run()
+    PointLabeler("../data", scale=0.55).run()
     print("Done!")
